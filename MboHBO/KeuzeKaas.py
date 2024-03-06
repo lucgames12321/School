100baf2481dce305d6d938130570273b70155fcc
lucgames12321 <lucstoffels2005@gmail.com>
2024-03-06 11:51:21 +0100

    New excercises

---
+++ MboHBO/KeuzeKaas.py
@@ -0,0 +1,15 @@
+print ("Ik heb kaas nodig. Welke kaas is lekker? (Kies uit: Jonge, Oude of Geiten)")
+
+keuze = input()
+
+if keuze == "Jonge":
+    print("Jonge kaas is inderdaar verrukkelijk")
+
+elif keuze == "Oude":
+    print("Oude kaas is smikkeltastisch")
+
+elif keuze == "Geiten":
+    print("Geiten kaas? Wie eet dit nou?")
+
+else: 
+    print("Je hebt misschien een typo gemaakt. (Misschien hoofdletter vergeten....)")
---
+++ MboHBO/itteratie.py
@@ -0,0 +1,10 @@
+# Variables, dus overal waar je a invult betekend dat 5. en zelfde voor b met -5.
+a = 5
+b = -5
+
+# Zolang a en b onder 10 zitten run de volgende code
+while a <= 10 and b <= 10:
+    print(f"a = {a}",f"b = {b}")
+    a = a+1
+    b = b+1
+print ("progamma geexecuteerd door Master Oogway.")
